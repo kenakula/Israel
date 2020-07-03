a0a7100885d523e79622e6538053bf2471a678ca
Kenan Agaev <kenkosh@yandex.ru>
2020-07-03 23:56:25 +0300

    minor changes

--- build/js/main.js
+++ build/js/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 (function () {
+  var TEL_REG_EXP = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+  var ESC_KEY = 'Escape';
+  var USER_OBJECT_NAME = 'user';
   var SCROLL_X = 0;
   var SCROLL_BEHAVIOUR = 'smooth';
   var STORAGE_NAME_VALUE = 'name';
   var STORAGE_PHONE_NUMBER = 'tel';
+  // var PHONE_NUMBER_MASK = '+{7} (000) 000 00 00';
+  var MIN_NAME_CHARS = 2;
 
   var orderButton = document.querySelector('.page-header__button');
   var orderModal = document.querySelector('.modal--callback');
@@ -44,9 +49,35 @@
 
   document.body.classList.remove('no-js');
 
-  // вставляет данные из Localstorage
+  // -------------------------------------- Localstorage
+
+  var setLocalStorage = function (key, value, userObj) {
+    userObj[key] = value;
+    localStorage.setItem(USER_OBJECT_NAME, JSON.stringify(userObj));
+  };
+
+  var getLocalStorageValue = function (key) {
+    return JSON.parse(localStorage.getItem(key));
+  };
+
+  var setInputValuesFromLocalStorage = function (phoneInputs, textInputs) {
+    var data = getLocalStorageValue(USER_OBJECT_NAME);
+
+    if (data) {
+      phoneInputs.forEach(function (it) {
+        it.value = data.tel;
+      });
+
+      if (data.name !== undefined) {
+        textInputs.forEach(function (it) {
+          it.value = data.name;
+        });
+      }
+    }
+  };
+
   var onPageLoadSetInputsValues = function () {
-    window.vendor.setInputValuesFromLocalStorage(telInputs, nameInputs);
+    setInputValuesFromLocalStorage(telInputs, nameInputs);
   };
 
   // -------------------------------------- скролл
@@ -63,22 +94,102 @@
 
   // -------------------------------------- модалки
 
+  var closeModal = function (modal) {
+    if (modal.classList.contains('modal--show')) {
+      modal.classList.remove('modal--show');
+      document.body.classList.remove('no-scroll');
+    }
+  };
+
+  var onEscButtonPressCloseModal = function (evt, modal) {
+    if (evt.key === ESC_KEY) {
+      closeModal(modal);
+      document.removeEventListener('keydown', onEscButtonPressCloseModal);
+    }
+  };
+
+  var onOverlayClickCloseModal = function (evt, modal) {
+    var modalInner = modal.querySelector('.modal__inner');
+    if (evt.target === modal && evt.target !== modalInner) {
+      closeModal(modal);
+      document.removeEventListener('keydown', onEscButtonPressCloseModal);
+    }
+  };
+
+  var showModal = function (modal) {
+    if (!modal.classList.contains('modal--show')) {
+      var closeButtons = modal.querySelectorAll('button[name="closeButton"]');
+
+      modal.classList.add('modal--show');
+      document.body.classList.add('no-scroll');
+
+      closeButtons.forEach(function (it) {
+        it.addEventListener('click', function () {
+          closeModal(modal);
+        });
+      });
+
+      modal.addEventListener('click', function (evt) {
+        onOverlayClickCloseModal(evt, modal);
+      });
+
+      document.addEventListener('keydown', function (evt) {
+        onEscButtonPressCloseModal(evt, modal);
+      });
+    }
+  };
+
   var onOrderButtonClickShowModal = function () {
-    window.vendor.showModal(orderModal);
+    showModal(orderModal);
     orderModal.querySelector('input[type="text"]').focus();
   };
 
-  // -------------------------------------- валидация
+  // -------------------------------------- формы и валидация
 
-  // TODO примени маски
-
-  var onTelInputChangeValidateValue = function (evt) {
-    window.vendor.validateTelNumber(evt.target, errors);
+  // сбрасывает стили ошибки
+  var resetInputError = function (input) {
+    if (input.parentElement.classList.contains('form__inner--error')) {
+      input.parentElement.classList.remove('form__inner--error');
+    }
   };
 
-  // при фокусе на инпут сбрасывает стили ошибки
-  var onTelInputFocusResetValue = function (evt) {
-    window.vendor.resetInputError(evt.target);
+  // устанавливает стили ошибки
+  var setErrorStyle = function (input) {
+    input.parentElement.classList.add('form__inner--error');
+  };
+
+  // валидация номера телефона
+  var validateTelNumber = function (input, errorsObj) {
+    var value = input.value;
+
+    if (!TEL_REG_EXP.test(value)) {
+      setErrorStyle(input);
+      errorsObj.tel = 'wrong tel';
+    } else {
+      resetInputError(input);
+      delete errorsObj.tel;
+    }
+  };
+
+  // валидация поля имени
+  var validateName = function (input, errorsObj) {
+    var value = input.value;
+
+    if (value.length < MIN_NAME_CHARS) {
+      setErrorStyle(input);
+      errorsObj.name = 'wrong name';
+    } else {
+      resetInputError(input);
+      delete errorsObj.name;
+    }
+  };
+
+  var onTelInputChangeValidateValue = function (evt) {
+    validateTelNumber(evt.target, errors);
+  };
+
+  var onNameInputChangeValidateValue = function (evt) {
+    validateName(evt.target, errors);
   };
 
   // получает значения инпута имени
@@ -96,16 +207,16 @@
   };
 
   var onFormSubmit = function (evt) {
-    if (errors.tel) {
+    if (errors.tel || errors.name) {
       evt.preventDefault();
     } else {
       evt.preventDefault();
-      window.vendor.closeModal(orderModal);
-      window.vendor.showModal(successModal);
-      window.vendor.setLocalStorage(STORAGE_PHONE_NUMBER, getPhoneInputValue(evt.target), user);
+      closeModal(orderModal);
+      showModal(successModal);
+      setLocalStorage(STORAGE_PHONE_NUMBER, getPhoneInputValue(evt.target), user);
       // проверяет наличие поля имени в форме
       if (evt.target.querySelector('input[type="text"]')) {
-        window.vendor.setLocalStorage(STORAGE_NAME_VALUE, getNameInputValue(evt.target), user);
+        setLocalStorage(STORAGE_NAME_VALUE, getNameInputValue(evt.target), user);
       }
 
       evt.target.reset();
@@ -182,11 +293,28 @@
     });
   }
 
+  // получает активный таб
+  var getActiveTab = function (container) {
+    return container.querySelector('.tabs__item--active');
+  };
+
+  // устанавливает активный таб
+  var setNewTab = function (currentIndex, newIndex, tabs, contents) {
+    var currentTab = tabs[currentIndex];
+    var currentContent = contents[currentIndex];
+
+    currentTab.classList.remove('tabs__item--active');
+    tabs[newIndex].classList.add('tabs__item--active');
+
+    currentContent.classList.remove('tabs__description--active');
+    contents[newIndex].classList.add('tabs__description--active');
+  };
+
   var onTabButtonClickContentShow = function (evt) {
-    var activeTabIndex = window.vendor.getActiveTab(tabsContainer).dataset.tab;
+    var activeTabIndex = getActiveTab(tabsContainer).dataset.tab;
     var newTabIndex = evt.currentTarget.dataset.tab;
 
-    window.vendor.setNewTab(activeTabIndex, newTabIndex, tabButtons, tabContents);
+    setNewTab(activeTabIndex, newTabIndex, tabButtons, tabContents);
   };
 
   // TODO центрирование таба в окне при его выборе
@@ -194,6 +322,12 @@
   // -------------------------------------- слайдер
 
   // israel slider
+
+  // меняет слайд
+  var changeSlide = function (slides, bullets, newIndex) {
+    slides[newIndex].classList.add('cards__img--active');
+    bullets[newIndex].classList.add('cards__bullet--active');
+  };
 
   // получает активный слайд
   var getActiveSlide = function (container) {
@@ -205,6 +339,7 @@
     return container.querySelector('.cards__bullet--active');
   };
 
+  // при клике на кнопку контрола меняет слайд
   var onBulletClickChangeIsraelSlide = function (evt) {
     var activeSlide = getActiveSlide(israelSlidesContainer);
     var activeBullet = getActiveBullet(israelSlidesContainer);
@@ -213,23 +348,75 @@
     activeSlide.classList.remove('cards__img--active');
 
     var newIndex = evt.target.dataset.index;
-
-    window.vendor.changeSlide(israelSlides, israelSlidesBullets, newIndex);
+    changeSlide(israelSlides, israelSlidesBullets, newIndex);
   };
 
   // testimonials slider
 
-  // устанавливает количество слайдов в индикатор
+  // получает текущий активный слайд
+  var getActiveTestimonialSlide = function (container) {
+    return container.querySelector('.testimonials__item--active');
+  };
+
+  // устанавливает количество слайдов в индикатор общего количества слайдов
   totalPagesIndicator.textContent = testimonials.length;
 
+  // меняет индикатор текущего слайда
+  var changeCurrentPageIndicator = function (indicator, slidesContainer) {
+    var currentSlide = getActiveTestimonialSlide(slidesContainer);
+    var currentPage = +currentSlide.dataset.slide + 1;
+    indicator.textContent = currentPage;
+  };
+
+  // получает индекс нового слайда
+  var getNewSlideIndex = function (direction, slides, container) {
+    var currentSlide = getActiveTestimonialSlide(container);
+    var newIndex;
+
+    switch (direction) {
+      case 'leftButton':
+        newIndex = +currentSlide.dataset.slide - 1;
+        break;
+      default:
+        newIndex = +currentSlide.dataset.slide + 1;
+    }
+
+    if (newIndex < 0) {
+      newIndex = slides.length - 1;
+    } else if (newIndex > slides.length - 1) {
+      newIndex = 0;
+    }
+
+    return newIndex;
+  };
+
+  // меняет слайды
+  var changeTestimonialSlide = function (container, slides, newIndex) {
+    var currentSlide = getActiveTestimonialSlide(container);
+    currentSlide.classList.remove('testimonials__item--active');
+    slides[newIndex].classList.add('testimonials__item--active');
+  };
+
+  // меняет слайды при использовании стрелок
   var onArrowClickChangeTestimonialSlide = function (evt) {
     var direction = evt.target.id;
-    var newSlideIndex = window.vendor.getNewSlideIndex(direction, testimonials, testimonialsContainer);
-    window.vendor.changeTestimonialSlide(testimonialsContainer, testimonials, newSlideIndex);
-    window.vendor.changeCurrentPageIndicator(currentPageIndicator, testimonialsContainer);
+    var newSlideIndex = getNewSlideIndex(direction, testimonials, testimonialsContainer);
+    changeTestimonialSlide(testimonialsContainer, testimonials, newSlideIndex);
+    changeCurrentPageIndicator(currentPageIndicator, testimonialsContainer);
   };
 
   // -------------------------------------- accordeon
+
+  // разворачивает контент таба
+  var showAccordeonContent = function (container, items, newIndex) {
+    var activeTab = container.querySelector('.accordeon__item--active');
+
+    if (activeTab) {
+      activeTab.classList.remove('accordeon__item--active');
+    }
+
+    items[newIndex].classList.add('accordeon__item--active');
+  };
 
   var onAccordeonButtonClickToggleContent = function (evt) {
     var newIndex = evt.target.dataset.content;
@@ -238,7 +425,7 @@
     if (elParent.classList.contains('accordeon__item--active')) {
       elParent.classList.remove('accordeon__item--active');
     } else {
-      window.vendor.showAccordeonContent(accordeon, accordeonItems, newIndex);
+      showAccordeonContent(accordeon, accordeonItems, newIndex);
     }
 
   };
@@ -271,7 +458,10 @@
 
   telInputs.forEach(function (it) {
     it.addEventListener('change', onTelInputChangeValidateValue);
-    it.addEventListener('focus', onTelInputFocusResetValue);
+  });
+
+  nameInputs.forEach(function (it) {
+    it.addEventListener('change', onNameInputChangeValidateValue);
   });
 
   forms.forEach(function (it) {
